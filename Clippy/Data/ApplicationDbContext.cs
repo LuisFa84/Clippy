a5ff34233a520ed8c817c18ee5964008fc50f00c
LuisFa84 <lfalvarez@outlook.com.ar>
2022-11-16 00:14:17 -0300

    Agregar archivos de proyecto.

---
+++ Clippy/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Clippy.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clippy.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //modelos
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Clippy/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace Clippy.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido es obligatorio")]
+        public string? Apellido { get; set; }
+        
+        [Required(ErrorMessage = "El Contacto es obligatorio")]
+        public string? Contacto { get; set; }
+        
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        public string? Email { get; set; }
+        
+    }
+}
